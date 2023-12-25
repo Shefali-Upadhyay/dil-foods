e54069d11b2c76ca5bd111bca961290b48db68ba
Shefali Upadhyay <shefaliupadhyay@Shefalis-MacBook-Air.local>
2023-12-25 13:51:43 +0530

    Added Navigation Bar

---
+++ src/app/components/Navbar/NavBar.tsx
@@ -0,0 +1,75 @@
+"use client"; 
+import Link from "next/link";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: "/",
+      name: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+      name: "about",
+    },
+    {
+      id: 3,
+      link: "store",
+      name: "store",
+    }
+  ];
+
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
+      <ul className="hidden md:flex">
+        {links.map(({ id, link, name }) => (
+          <li
+            key={id}
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
+          >
+            <Link href={link}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link, name }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setNav(!nav)} href={link}>
+                {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className="flex justify-center items-center">
+        <div className="relative py-2">
+        <div className="t-0 absolute left-3">
+          <p className="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">3</p>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="file: mt-4 h-6 w-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+        </svg>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
